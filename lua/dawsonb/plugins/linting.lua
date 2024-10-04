404ffc19963e2434f01e2e6bc2aadb321aa9aeda
Dawson Burgess <dawsonhburgess@gmail.com>
2024-10-03 19:59:46 -0700

    remove c linting

--- lua/dawsonb/plugins/linting.lua
+++ lua/dawsonb/plugins/linting.lua
@@ -7,8 +7,6 @@ return {
 		lint.linters_by_ft = {
 			javascript = { "eslint_d" },
 			typescript = { "eslint_d" },
-			c = { "cpplint" },
-			cpp = { "cpplint" },
 		}
 
 		local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
