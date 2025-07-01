ecda57097f231390b4db10aa11a4e8a6c3506aec
dawsonb <dawsonhburgess@gmail.com>
2025-07-01 12:07:58 -0700

    changed colorscheme
    to rose pine moon. hopefully it stays this way lol

--- lua/hades/plugins/colorscheme.lua
+++ lua/hades/plugins/colorscheme.lua
@@ -1,17 +1,36 @@
-return {
-  "folke/tokyonight.nvim",
-  priority = 1000,
-  lazy = false,
-  config = function()
-    require("tokyonight").setup({
-      style = "night",
-      transparent = false,
-      styles = {
-        sidebars = "transparent", --or "dark",
-        floats = "transparent", --or "dark",
-      },
-    })
+-- return {
+--   "folke/tokyonight.nvim",
+--   priority = 1000,
+--   lazy = false,
+--   config = function()
+--     require("tokyonight").setup({
+--       style = "night",
+--       transparent = false,
+--       styles = {
+--         sidebars = "transparent", --or "dark",
+--         floats = "transparent", --or "dark",
+--       },
+--     })
+-- 
+--     vim.cmd("colorscheme tokyonight-night")
+--   end,
+-- }
 
-    vim.cmd("colorscheme tokyonight-night")
-  end,
+-- return {
+--   "rebelot/kanagawa.nvim",
+--   priority = 1000,
+--   lazy = false,
+--   config = function()
+--     vim.cmd("colorscheme kanagawa-wave") -- dragon or wave
+--   end,
+-- } 
+--
+--
+-- lua/plugins/rose-pine.lua
+return {
+	"rose-pine/neovim",
+	name = "rose-pine",
+	config = function()
+		vim.cmd("colorscheme rose-pine-moon")
+	end
 }
