450e615befd7795add77cdcdcce9c6d3c59432fe
dawsonb <dawsonhburgess@gmail.com>
2025-07-01 12:07:15 -0700

    added telescope commands
    added live grep, commands search

--- lua/hades/plugins/telescope.lua
+++ lua/hades/plugins/telescope.lua
@@ -4,7 +4,9 @@ return {
   -- replace all Telescope keymaps with only one mapping
   keys = function()
     return {
-      { "<leader>ff", "<cmd>Telescope find_files<cr>", desc = "Find Files" },
+      { "<leader>ff", "<cmd>Telescope find_files<cr>", desc = "Telescope Find Files" },
+      { "<leader>fg", "<cmd>Telescope live_grep<cr>", desc = "Telescope Live Grep" },
+      { "<leader>fc", "<cmd>Telescope commands<cr>", desc = "Telescope Commands" },
     }
   end,
 }
