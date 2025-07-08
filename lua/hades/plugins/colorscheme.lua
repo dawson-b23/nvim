13a2a2ff7ee4f2765589f2dfb69a088e83c27f60
dawsonb <dawsonhburgess@gmail.com>
2025-07-08 10:19:38 -0700

    changes....
    zellij nav yay
    changed to kanagawa theme

--- lua/hades/plugins/colorscheme.lua
+++ lua/hades/plugins/colorscheme.lua
@@ -16,21 +16,31 @@
 --   end,
 -- }
 
--- return {
---   "rebelot/kanagawa.nvim",
---   priority = 1000,
---   lazy = false,
---   config = function()
---     vim.cmd("colorscheme kanagawa-wave") -- dragon or wave
---   end,
--- } 
+return {
+  "rebelot/kanagawa.nvim",
+  priority = 1000,
+  lazy = false,
+  config = function()
+    vim.cmd("colorscheme kanagawa-wave") -- dragon or wave
+  end,
+} 
 --
 --
 -- lua/plugins/rose-pine.lua
-return {
-	"rose-pine/neovim",
-	name = "rose-pine",
-	config = function()
-		vim.cmd("colorscheme rose-pine-moon")
-	end
-}
+-- return {
+-- 	"rose-pine/neovim",
+-- 	name = "rose-pine",
+-- 	config = function()
+-- 		vim.cmd("colorscheme rose-pine-moon")
+-- 	end
+-- }
+
+--return {
+--  "olimorris/onedarkpro.nvim",
+--  priority = 1000, -- Ensure it loads first
+--  lazy = false,
+--  config = function()
+--    vim.cmd("colorscheme onedark")
+--  end
+--}
+
